aca7bcfee24ed0df2ba5f340f61d61f16d55a6a9
Nikhita Kamath <nikhita.kamath@gmail.com>
2016-03-09 18:52:53 -0500

    Add method for new profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,21 @@
 class ProfilesController < ApplicationController
-	def index
+	
+  def index
 	end
 
-	def new
-	end
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def new
+    @profile = Profile.new 
+  end
+
+  def create
+    binding.pry
+    @user = current_user
+    @user.profile.create (params[:user_id])
+    redirect_to user.profile
+  end
 
 end
