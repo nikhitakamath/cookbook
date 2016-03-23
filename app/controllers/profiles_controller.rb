1802864c50a073573e1ff1688c0b0012e6f74ebc
Nikhita Kamath <nikhita.kamath@gmail.com>
2016-03-23 18:09:29 -0400

    Add save and show on Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,13 @@
 class ProfilesController < ApplicationController
-	
+
+  respond_to :html
+
   def index
 	end
 
   def show
-    profile = Profile.find(params[:id])
+    @profile = Profile.find(params[:id])
+    respond_with(@profile)
   end
 
   def new
@@ -13,14 +16,30 @@ class ProfilesController < ApplicationController
 
   def create
     # profile = Profile.new(user: current_user)
-
-    profile = Profile.new
-    profile.user = current_user
-    profile.save
-
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    @profile.save
     #@user = current_user
     #@user.profile.create (params[:user_id])
     #redirect_to user.profile
+  end
+
+
+private
+
+  def profile_params
+    params.require(:profile).permit(
+      :name,
+      :start_date, 
+      :team_captain, 
+      :address, 
+      :working_status, 
+      :vip_status, 
+      :title, 
+      :lyft, 
+      :pam, 
+      :endzone
+      )
   end
 
 end
